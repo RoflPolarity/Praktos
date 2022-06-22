6b6c333f672180e4ad3018fdaf8bc7396c827b5e
RoflPolarity <ignis604@gmail.com>
2022-06-22 16:52:20 +0300

    small fix

--- src/main/java/Veivlet.java
+++ src/main/java/Veivlet.java
@@ -2,34 +2,18 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Random;
 
 public class Veivlet {
     public static void main(String[] args) throws IOException {
-        File picPath = new File("car.jpg");
-        System.out.println(picPath.getAbsolutePath());
-        BufferedImage pic = ImageIO.read(picPath);
-        int[][] pixels = new int[pic.getWidth()][pic.getHeight()];
-        for (int i = 0; i < pixels.length; i++) {
-            for (int j = 0; j < pixels[i].length; j++) {
-                pic.setRGB(i,j,MassNoise(5,pic)[i][j]);
-            }
-        }
-        File noise = new File("noise.jpg");
-        ImageIO.write(pic,"jpg",noise);
-        File norm = new File("norm.jpg");
-        ImageIO.write(NormFactor(pic),"jpg",norm);
+        BufferedImage pic = ImageIO.read(new File("car.jpg"));
+        ImageIO.write(MassNoise(1,pic),"jpg",new File("noise.jpg"));
+        ImageIO.write(NormFactor(pic),"jpg",new File("norm.jpg"));
     }
-    public static int[][] MassNoise(int sigma, BufferedImage pic){
+    public static BufferedImage MassNoise(int sigma, BufferedImage pic){
         Random rand = new Random();
-        int[][] res = new int[pic.getHeight()][pic.getWidth()];
-        for (int i = 0; i < res.length; i++) {
-            for (int j = 0; j < res[i].length; j++) {
-                res[i][j] = (int) (Math.abs(pic.getRGB(i,j)) + sigma*rand.nextGaussian());
-            }
-        }
-        return res;
+        for (int i = 0; i < pic.getWidth(); i++)for (int j = 0; j < pic.getHeight(); j++) pic.setRGB(i,j, (int) (Math.abs(pic.getRGB(i,j)) + sigma*rand.nextGaussian()));
+        return pic;
     }
     public static BufferedImage NormFactor(BufferedImage pic){
         int min = Math.abs(pic.getRGB(0,0)), max = Math.abs(pic.getRGB(0,0));
